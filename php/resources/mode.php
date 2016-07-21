ce5821dd4ff3caeb80558d6ffe251490426a4eb3
desutton <dsutton@desmail.com>
2016-07-21 09:16:15 -0500

    Abadoning the Tree concept

--- php/resources/mode.php
+++ php/resources/mode.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * <dsCode> Inc. (c) 2016. This copyright is based on the Apache License 2.0. Please contact David Sutton for use of this software.
+ * This script will activate the system behavior for Development, Q/A, or Production.
  */
 
 $sysmode = "dev";
