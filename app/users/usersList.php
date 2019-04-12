c9510aa389c92eebc24cce1da8fc7bf24cc2f4ee
David <dsutton@desmail.com>
2019-04-12 16:13:45 -0700

    Abadoning the Tree concept

---
+++ app/users/usersList.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * <dsCode> Inc. (c) 2016. This copyright is based on the Apache License 2.0. Please contact David Sutton for use of this software.
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: sutton
+ * Date: 8/25/16
+ * Time: 8:32 PM
+ */
+include $_SERVER['DOCUMENT_ROOT'] . '/labs2/php/header.php'; ?>
+
+Users List Page
+<script src="usersList.js" type="text/javascript"></script>
+</body>
+</html>
