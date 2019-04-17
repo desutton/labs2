4624027716372de4a56768c68f092572922a544e
David <dsutton@desmail.com>
2019-04-16 23:26:46 -0700

    Working on User panels

--- app/users/usersAccount.js
+++ app/users/usersAccount.js
@@ -2,7 +2,7 @@
  * <dsCode> Inc. (c) 2019. This copyright is based on the Apache License 2.0. Please contact David Sutton for use of this software.
  */
 
-var useruuid = 'xxxxxxxx-xxxx-5xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+var $useruuid = 'xxxxxxxx-xxxx-5xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = Math.random() * 16 | 0, v = c === 'x' ? r : r & 0x3 | 0x8;
     return v.toString(16);
 });
@@ -14,10 +14,10 @@ var tabbar = {
     view: "toolbar",
     id: "userProfileSettingsToolbar",
     elements: [
-        {view: "button", value: "Add", width: 70, click: "add_row"},
+        //{view: "button", value: "Add", width: 70, click: "add_row"},
         {view: "button", value: "Delete", width: 70, click: "delete_row"},
         {view: "button", value: "Modify", width: 70, click: "update_row"},
-        {view: "button", value: "Reset", width: 85, click: "$$('userDetail').clear()"}
+        {view: "button", value: "Reset", width: 85, click: "refresh_row"}
     ]
 };
 
@@ -113,24 +113,48 @@ webix.ui({
 
 }).show();
 
-
-webix.ajax().get("/labs2/php/api_methods/SELECTz.php?tableName=users&columnNames=users_firstname,users_lastname,users_name,users_displayName,users_pass,users_employeeID&selectColumn=users_name&selectData=" + $userId + "&dataName=data&select=1", function (text, data) {
-    var userAuthor = text;
-    userAuthor = userAuthor.replace('[', "");
-    userAuthor = userAuthor.replace(']', "");
-    userAuthor = JSON.parse(userAuthor);
-    $$('users_firstname').setValue(userAuthor.users_firstname);
-    $$('users_lastname').setValue(userAuthor.users_lastname);
-    $$('users_name').setValue(userAuthor.users_name);
-    $$('users_displayName').setValue(userAuthor.users_displayName);
-    $$('users_employeeId').setValue(userAuthor.users_employeeID);
-    $$('users_pass').setValue(userAuthor.users_pass);
-    $$('users_UUID').setValue(userAuthor.users_UUID);
-
+$$("userDetail").load(function () {
+    webix.message("Data loaded");
+    window.setTimeout(refresh_row, 100);
 });
 
 function delete_row() {
     console.log("close window");
+    webix.message({text: "Deleted"}); //Optional UI to display that something happened
+    window.setTimeout(refresh_row, 1000);
+}
+
+function update_row() {
+    var firstname = $$("users_firstname").getValue();
+    var lastname = $$("users_lastname").getValue();
+    var username = $$("users_name").getValue();
+    var displayname = $$("users_displayName").getValue();
+    var employeeid = $$("users_employeeId").getValue();
+    var userpass = $$("users_pass").getValue();
+    var useruuid = $$("users_UUID").getValue();
+    var dataSumit2Users = '{"users_firstname":"' + firstname + '", "users_lastname":"' + lastname + '", "users_name":"' + username + '", "users_displayName":"' + displayname + '", "users_employeeId":"' + employeeid + '", "users_pass":"' + userpass + '"}';
+    webix.ajax("/labs2/php/api_methods/UPDATEz.php?tableName=users&JSONdata=" + dataSumit2Users + "&theWhereColumn=users_UUID&theUUID=" + useruuid);
+//    console.log("JSON DATA being sent to the server " + theSubmitDataRAW2); //just a debug code
+    webix.message({text: "Saved"}); //Optional UI to display that something happened
+    window.setTimeout(refresh_row, 1000);
+}
+
+function refresh_row() {
+    //console.log("hello");
+    webix.message({text: "Reloading..."});
+    webix.ajax().get("/labs2/php/api_methods/SELECTz.php?tableName=users&columnNames=users_firstname,users_lastname,users_name,users_displayName,users_pass,users_employeeID,users_UUID&selectColumn=users_name&selectData=" + $userId + "&dataName=data&select=1", function (text, data) {
+        var userAuthor = text;
+        userAuthor = userAuthor.replace('[', "");
+        userAuthor = userAuthor.replace(']', "");
+        userAuthor = JSON.parse(userAuthor);
+        $$('users_firstname').setValue(userAuthor.users_firstname);
+        $$('users_lastname').setValue(userAuthor.users_lastname);
+        $$('users_name').setValue(userAuthor.users_name);
+        $$('users_displayName').setValue(userAuthor.users_displayName);
+        $$('users_employeeId').setValue(userAuthor.users_employeeID);
+        $$('users_pass').setValue(userAuthor.users_pass);
+        $$('users_UUID').setValue(userAuthor.users_UUID);
+    });
 }
 
 function add_row() {
@@ -140,16 +164,9 @@ function add_row() {
     var displayname = $$("users_displayName").getValue();
     var employeeid = $$("users_employeeId").getValue();
     var userpass = $$("users_pass").getValue();
-    var dataSumit2Users = '{"success":true,"data":[{"users_UUID":"' + useruuid + '", "users_firstname":"' + firstname + '", "users_lastname":"' + lastname + '", "users_name":"' + username + '", "users_displayName":"' + displayname + '", "users_employeeId":"' + employeeid + '", "users_pass":"' + userpass + '"}]}';
+    var dataSumit2Users = '{"success":true,"data":[{"users_UUID":"' + $useruuid + '", "users_firstname":"' + firstname + '", "users_lastname":"' + lastname + '", "users_name":"' + username + '", "users_displayName":"' + displayname + '", "users_employeeId":"' + employeeid + '", "users_pass":"' + userpass + '"}]}';
     webix.ajax("/labs2/php/api_methods/INSERT.php?tableName=requisitionRows&JSONdata=" + dataSumit2Users);
 //    console.log("JSON DATA being sent to the server " + theSubmitDataRAW2); //just a debug code
     webix.message({text: "Saved"}); //Optional UI to display that something happened
-    window.setTimeout(refreshTheUserData, 1000);
-    webix.message({text: "Reloading..."});
+    window.setTimeout(refresh_row, 1000);
 }
-
-function refreshTheUserData() {
-    console.log("hello");
-
-}
-
